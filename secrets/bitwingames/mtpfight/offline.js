69ff58a7a68308196a85bfb5c6855a645a4c5a0e
Pedro Braga <mrhenribraga@gmail.com>
2022-11-12 22:27:47 -0300

    Added bitwingames to secrets folder.

---
+++ secrets/bitwingames/mtpfight/offline.js
@@ -0,0 +1,27 @@
+﻿{
+	"version": 1509041018,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/tiledbackground.png",
+		"images/fighter-sheet0.png",
+		"images/fighter-sheet1.png",
+		"images/fighter-sheet2.png",
+		"images/playerindicator-sheet0.png",
+		"images/playerindicator-sheet1.png",
+		"images/damage-sheet0.png",
+		"images/shade-sheet0.png",
+		"images/bg-sheet0.png",
+		"images/bg-sheet1.png",
+		"images/bg-sheet2.png",
+		"images/lifecontainer-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
