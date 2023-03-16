35dd7f7dc39e0342ea8643cb45d0f6f071672d20
mrpedrobraga <mrhenribraga@gmail.com>
2023-03-16 18:27:30 -0300

    Addded navigation icons to the gallery

--- inner_voices/gallery.js
+++ inner_voices/gallery.js
@@ -2,6 +2,10 @@
 var btn_next = document.querySelector(".next")
 var btn_prev = document.querySelector(".previous")
 var gallery_container = document.querySelector(".gallery-container")
+var gallery_counter = document.querySelector(".gallery-counter")
+
+var gallery_elements = []
+var gallery_counter_elements = []
 
 gallery_index = 0
 gallery_max_index = 5
@@ -10,6 +14,7 @@ var gallery_img_list = [
 	"../gallery/img/invo_new_horizon_heroes.png",
 	"../gallery/img/invo_ollie_room.png",
 	"../gallery/img/invo_vs_magnum_opus_1.png",
+	"../gallery/img/invo_lost_massie.png",
 	"../gallery/img/invo_mystery_chat.png",
 	"../gallery/img/invo_hank_minecart.png",
 	"../gallery/img/invo_twisted_school_0.png",
@@ -20,29 +25,42 @@ var gallery_img_list = [
 var populate_gallery = () => {
 	gallery_img_list.forEach((el, index) => {
 		var img = document.createElement("img")
-		gallery_container.appendChild(img)
-
 		img.src = el
+		gallery_container.appendChild(img)
+		gallery_elements.push(img)
+
+		//<div class="gallery-counter-item"></div>
+		var img_counter = document.createElement("div")
+		img_counter.classList.add("gallery-counter-item")
+		img_counter.onclick = () => {set_gallery_item(index)}
+		gallery_counter.appendChild(img_counter)
+		gallery_counter_elements.push(img_counter)
 	})
-	gallery_max_index = gallery_img_list.length
+	gallery_max_index = gallery_img_list.length - 1
 }
 
 var next_gallery_item = () => {
-	if (gallery_index < gallery_max_index)
-		gallery_index++
-	
+	if (gallery_index < gallery_max_index) gallery_index++
 	gallery_adjust_scroll()
 }
 
 var prev_gallery_item = () => {
-	if (gallery_index > 0)
-		gallery_index--
-	
+	if (gallery_index > 0) gallery_index--
+	gallery_adjust_scroll()
+}
+
+var set_gallery_item = (index) => {
+	gallery_index = index
 	gallery_adjust_scroll()
 }
 
 var gallery_adjust_scroll = () => {
-	gallery_container.scroll(gallery_container.clientWidth * gallery_index, 0)
+	gallery_elements[gallery_index].scrollIntoView({block: "nearest", inline: "nearest"})
+	gallery_counter_elements.forEach((el,index)=> {
+		el.classList.remove('current')
+		if (index == gallery_index)
+			el.classList.add('current')
+	})
 }
 
 addEventListener("load", setup)
@@ -52,6 +70,5 @@ addEventListener("load", gallery_adjust_scroll)
 var setup = () => {
 	btn_prev.onclick = prev_gallery_item
 	btn_next.onclick = next_gallery_item
-
 	gallery_adjust_scroll()
 }
