01a1c5e56e9af6797730e065e66ffcf530104da4
mrpedrobraga <mrhenribraga@gmail.com>
2023-10-21 17:39:06 -0300

    Somewhat initialized repo.

---
+++ nano/highlight_nano.js
@@ -0,0 +1,80 @@
+const STRING_DQ_MODE = {
+    scope: "string",
+    begin: '"',
+    end: '"',
+    contains: [
+      { scope: "char.escape", match: "\\\\." },
+      { scope: "subst", begin: "\\{", end: "\\}" },
+    ],
+  };
+  
+  const STRING_SQ_MODE = {
+    scope: "string",
+    begin: "'",
+    end: "'",
+    contains: [
+      { scope: "char.escape", match: "\\\\." },
+      { scope: "subst", begin: "\\{", end: "\\}" },
+    ],
+  };
+  
+  const STRING_GROUP = [STRING_DQ_MODE, STRING_SQ_MODE];
+  
+  const EXPRESSION_MODE = [
+    { scope: "variable.constant", match: "\\b[A-Z_][A-Z_]+\\b" },
+    { scope: "number", match: "\\b[\\d_]+(\\.[\\d+_])?f?\\b" },
+    { scope: "meta", match: "%%[\\w\\.]+" },
+    { scope: "meta", match: "#%[\\w\\.]+" },
+    STRING_DQ_MODE,
+    STRING_SQ_MODE,
+    {
+      scope: "regexp",
+      begin: "r/",
+      end: "/",
+    },
+    {
+      scope: "regexp",
+      begin: "o/",
+      end: "/",
+    },
+    hljs.COMMENT(
+      "###", // begin
+      "###" // end
+    ),
+    hljs.COMMENT(
+      "#", // begin
+      "\n" // end
+    ),
+  ];
+  
+  hljs.registerLanguage("nano", function () {
+    return {
+      unicodeRegex: true,
+      case_insensitive: false,
+      keywords: {
+        $pattern: /[\w_]+/,
+        keyword: `
+        if unless then elif elun else loop for while meanwhile until do select count reduce 
+        when match default 
+        type slot view signal state struct class trait entity object
+        print prompt exit err 
+        in of xis is not and or xor
+        constraint implies
+        typeof as keyof addrof 
+        let with shared import from use has await 
+        fn err_handler get set construct on
+        return err continue break discard yield
+        test assert 
+      `,
+        type: `byte bool bitfield uint int uvec2 ivec2 uvec3 ivec3 uvec4 ivec4 float fvec2 fvec3 fvec4 obj 
+        string array list list_view list_state addr 
+        i8 i32 i64 f32 f64 u8 u32 u64 option Expectation ErrorHandler ParseError 
+        `,
+        literal: `false true yes no PI TAU self selffn super superfn`,
+        punctuation: `( ) [ ] { } < >`,
+        operator: `+ - // / * >> << = == != | &`,
+      },
+      contains: EXPRESSION_MODE,
+    };
+  });
+  
